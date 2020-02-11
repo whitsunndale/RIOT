8dcbf8706ec6b0437ecf1a538965948000783c52
SAUVAGE Pierre-Jean <pierrejean.sauvage.etu@univ-lille.fr>
2020-02-11 16:56:51 +0100

    first driver version

---
+++ drivers/stbm271/include/stbm271_params.h
@@ -0,0 +1,26 @@
+#include "board.h" 
+#include "stbm271.h"
+
+/**
+ * @brief   Default configuration parameters for stbm271 sensor
+ * @{
+ */
+#ifndef STBM271_PARAM_GPIO1
+#define STBM271_PARAM_GPIO1        (GPIO_PIN(1,4))
+#endif
+#ifndef STBM271_PARAM_GPIO2
+#define STBM271_PARAM_GPIO2       (GPIO_PIN(1,5))
+#endif
+
+#ifndef STBM271_PARAMS
+#define STBM271_PARAMS           { .output1  = STBM271_PARAM_GPIO1, \
+                                  .output2 = STBM271_PARAM_GPIO2 }
+#endif
+/** @} */
+
+/**
+ * @brief   Allocation of stbm271 configuration
+ */
+static const stbm271_params_t stbm271_params[] = {
+    STBM271_PARAMS
+}
